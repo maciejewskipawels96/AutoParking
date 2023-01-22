7a2e856e6f5cf4f63409141aedd2e4bbbf2d069c
maciejewskipawels96 <s9699pm@ms.wwsi.edu.pl>
2023-01-22 11:59:28 +0100

    migration

---
+++ Migrations/20230122105801_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AutoParking.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Marka = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    pojemnoscsilnik = table.Column<int>(type: "int", nullable: false),
+                    Rokprod = table.Column<int>(type: "int", nullable: false),
+                    cena = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Autos");
+        }
+    }
+}
